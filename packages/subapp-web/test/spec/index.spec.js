891c50c6c6c414677b42b3ada0e0bb7f223e01f5
1846689910 <zuhe75@gmail.com>
2019-05-22 15:53:25 -0700

    index.spec

--- packages/subapp-web/test/spec/index.spec.js
+++ packages/subapp-web/test/spec/index.spec.js
@@ -3,7 +3,15 @@
 const subAppWeb = require("../..");
 
 describe("subapp-web", function() {
-  it("test", () => {
+  it("should export module", () => {
     expect(subAppWeb).to.exist;
+  });
+
+  it("dynamicLoadSubApp should return undefined", () => {
+    expect(subAppWeb.dynamicLoadSubApp()).to.not.exist;
+  });
+
+  it("getBrowserHistory should return undefined", () => {
+    expect(subAppWeb.getBrowserHistory()).to.not.exist;
   });
 });
