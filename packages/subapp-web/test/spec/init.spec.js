6030948a366676aa272f66f693a9e80a1a10b92f
1846689910 <zuhe75@gmail.com>
2019-05-23 09:47:21 -0700

    init

--- packages/subapp-web/test/spec/init.spec.js
+++ packages/subapp-web/test/spec/init.spec.js
@@ -1,17 +1,25 @@
 const init = require("../../lib/init");
 const Fs = require("fs");
 const Path = require("path");
+const sinon = require("sinon");
 const setupContext1 = require("../data/setup-context1.json");
 const setupContext2 = require("../data/setup-context2.json");
 
 describe("init", () => {
-
   it("should generate initial scripts for web page", () => {
+    const stubManifest = sinon
+      .stub(require("subapp-util"), "getAllSubAppManifest")
+      .callsFake(() => ({ server1: {}, server2: {} }));
+    const stubLoad = sinon
+      .stub(require("subapp-util"), "loadSubAppServerByName")
+      .callsFake(name => ({ name, initialize: () => {} }));
     const pkg = init(setupContext1);
     const content = Fs.readFileSync(Path.resolve("./browser/subapp-web.js"));
     const initContent = pkg.process();
     expect(initContent).to.include(`<script id="bundleAssets" type="application/json">`);
     expect(initContent).to.include(content);
+    stubLoad.restore();
+    stubManifest.restore();
   });
 
   it("should use dist/subapp-web.js if NODE_ENV=production", () => {
