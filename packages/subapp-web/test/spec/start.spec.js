0f156d72e96da68ba134dfa1c96b4fdc8dc214e5
1846689910 <zuhe75@gmail.com>
2019-05-24 08:55:37 -0700

    start.js

---
+++ packages/subapp-web/test/spec/start.spec.js
@@ -0,0 +1,8 @@
+const setup = require("../../lib/start");
+
+describe("start", () => {
+  it("should return the subapp start", () => {
+    const start = setup().process();
+    expect(start).to.equal("\n<!-- subapp start -->\n");
+  });
+});
